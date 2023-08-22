2c774e6e2a362c8334f8dd74460ddbff0c6116e9
KOCoders <74465408+KOCoders@users.noreply.github.com>
2023-08-22 21:00:26 +0700

    Add project files.

---
+++ ASP_NET_CORE_TEMPLATE/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using ASP_NET_CORE_TEMPLATE.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ASP_NET_CORE_TEMPLATE.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            StudentDetail studentDetail = new StudentDetail() { Name = "KT"};
+
+            return View(studentDetail);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ASP_NET_CORE_TEMPLATE/Models/StudentDetail.cs
@@ -0,0 +1,7 @@
+﻿namespace ASP_NET_CORE_TEMPLATE.Models
+{
+    public class StudentDetail
+    {
+        public string Name { get; set; }
+    }
+}
